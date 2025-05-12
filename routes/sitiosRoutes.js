a42e62b95bdfcc2da71adf61b8056e0c33eab813
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-05-12 14:05:56 -0300

    Ajustes sitiosRoutes.js

--- routes/sitiosRoutes.js
+++ routes/sitiosRoutes.js
@@ -1,1 +1,31 @@
-﻿// ============================// /routes/sitiosRoutes.js// ============================const express = require("express");const router = express.Router();const sitiosController = require("../controllers/sitiosController");// Ruta de bienvenidarouter.get("/", sitiosController.welcome);// Listado de sitios turÃ­sticosrouter.get("/sitios", sitiosController.getSitios);// Detalle por IDrouter.get("/sitios/:id", sitiosController.getSitioById);module.exports = router;
+﻿// ============================
+// /routes/sitiosRoutes.js
+// ============================
+const express = require("express");
+const router = express.Router();
+const sitiosController = require("../controllers/sitiosController");
+
+// Ruta de bienvenida
+router.get("/", sitiosController.welcome);
+
+// Listado de sitios turísticos
+router.get("/sitios", sitiosController.getSitios);
+
+// Detalle por ID
+router.get("/sitios/:id", sitiosController.getSitioById);
+
+module.exports = router;
+
+const fetch = require("node-fetch");
+
+exports.fetchJSON = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const data = await response.json();
+    throw {
+      status: response.status,
+      message: data.error || "Error al obtener datos externos",
+    };
+  }
+  return response.json();
+};
